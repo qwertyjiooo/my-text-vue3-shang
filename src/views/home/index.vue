4eaa32c880bea0df13122827bfa6c96ee5a15bb9
qwertyjiooo <143780130+qwertyjiooo@users.noreply.github.com>
2024-11-20 10:17:00 +0800

    111

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
     <el-button class="text-red-600" @click="aaa" :loading="loading">请求按钮</el-button>
     <el-button class="text-red-600" @click="bbb" :loading="loading">退出登录</el-button>
+    <!-- Math中提供的函数的功能 -->
+    <el-button class="text-red-600" @click="testMath" :loading="loading">Math</el-button>
     <ul>
         <li v-for="res in list" :key="res.id">
             <span>{{ res.first_name }}</span>
@@ -33,6 +35,9 @@ const aaa = async () => {
 const bbb = () => {
     localStorage.removeItem('token')
     router.push('/login')
+}
+const testMath = () => {
+    console.log(Math)
 }
 </script>
 
