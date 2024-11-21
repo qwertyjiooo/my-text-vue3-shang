21c975e554f9d42e3f8bdb31df02ec2b2d7c0080
Aerie <15938650343@163.com>
2024-11-21 22:05:46 +0800

    111
    2024.11.21 22：05

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="text-red-500">{{ userStore.name }}</div>
-    <el-button @click="userStore.setName('张三')">修改姓名</el-button>
+    <div class="content">
+        <div class="text-red-500">{{ userStore.name }}</div>
+        <Button @click="userStore.setName('张三')">
+            点击确定
+        </Button>
+    </div>
 </template>
 <script setup>
 import { useUserStore } from '@/store'
+import { Button } from '@/components/ui/button';
 
 const userStore = useUserStore()
 
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
