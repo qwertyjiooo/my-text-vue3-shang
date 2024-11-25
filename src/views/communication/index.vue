7fc2f26d77f0f45664a60a83b8f7c722bf859978
Aerie <15938650343@163.com>
2024-11-25 22:42:16 +0800

    2024/11/25 22：42

--- src/views/communication/index.vue
+++ src/views/communication/index.vue
@@ -1,29 +1,41 @@
 <template>
-    <div style="color: red;font-size: 24px">组件间通信</div>
-    <br>
-    <!-- 父组件向子组件传值 -->
-    <child :msg="msg" @greet = "handleGreet" />
-    <br>
-    <h1>子组件向父组件传值：{{gre}}</h1>
-    <br>
-    <!-- 透传Attributes($attrs) -->
-    <Attributes name="小明" age="18" hobby="篮球"/>
-    <br>
-    <!-- 获取子组件的引用 -->
-    <getRefs ref="childRef"/>
-    <el-button @click="callChildMethod">调用子组件方法</el-button>
+    <div style="padding-bottom: 40px;">
+        <div style="color: red;font-size: 24px">组件间通信</div>
+        <br>
+        <!-- 父组件向子组件传值 -->
+        <child :msg="msg" @greet="handleGreet" />
+        <br>
+        <h1>子组件向父组件传值：{{ gre }}</h1>
+        <br>
+        <!-- 透传Attributes($attrs) -->
+        <Attributes name="小明" age="18" hobby="篮球" />
+        <br>
+        <!-- 获取子组件的引用 -->
+        <getRefs ref="childRef" />
+        <el-button @click="callChildMethod">调用子组件方法</el-button>
+        <br>
+        <br>
+        <br>
+        <!-- v-model组件传值 -->
+        <vModel v-model:msg2="msg2" />
+        <br>
+        <br>
+        <p>父组件中：<br><br><el-input v-model="msg2" /></p>
+    </div>
+
 </template>
 
 <script setup>
 import child from './child.vue'
 import Attributes from './Attributes.vue'
 import getRefs from './getRefs.vue';
+import vModel from './vModel.vue';
 import { ref } from 'vue'
 // 父组件向子组件传值
 const msg = '父组件传给子组件的值'
 // 子组件向父组件传值
 const gre = ref('子组件还没问候父组件')
-const handleGreet  = (val) => {
+const handleGreet = (val) => {
     gre.value = val
 }
 // 获取子组件的引用
@@ -31,7 +43,8 @@ const childRef = ref(null)
 const callChildMethod = () => {
     childRef.value.someMethod()
 }
-
+// v-model
+const msg2 = ref('父组件中v-model绑定的值')
 </script>
 
 <style lang='less' scoped></style>
---
+++ src/views/communication/vModel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click="aaa">v-model 组件传值</div>
+    <br>
+    <el-input :value="msg2" @input="updateName"/>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const props = defineProps(['msg2'])
+const emit = defineEmits(['update:msg2']);
+const updateName = (e) => {
+    console.log(e);
+    // emit('update:msg2', e.target.value);
+}
+const aaa = () => {
+    console.log(props);
+}
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
